eef962c483fcf52e52efe6bb542f1dd37d74af17
B-NightShade <79291516+B-NightShade@users.noreply.github.com>
2022-02-23 14:27:40 -0700

    Initial commit jframe created in view

---
+++ src/Controller.java
@@ -0,0 +1,6 @@
+public class Controller {
+    public void go() {
+        View v1 = new View();
+        v1.main();
+    }
+}
---
+++ src/View.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class View {
+    public static void main() {
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+    }
+}
