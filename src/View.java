2f0b24b1eb004817995fc342b790b0263b0d2654
B-NightShade <79291516+B-NightShade@users.noreply.github.com>
2022-02-23 15:05:32 -0700

    add delete buttons panel to north

--- src/View.java
+++ src/View.java
@@ -1,8 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class View {
-    public static void main() {
+    public void main() {
+        JPanel myPanel = new JPanel();
+        JTextField jTextField = new JTextField(20);
+        myPanel.add(jTextField);
+        myPanel.add(new JButton("add"));
+        JButton del = new JButton("Delete");
         JFrame jFrame = new JFrame();
+        jFrame.add(BorderLayout.NORTH, myPanel);
+        jFrame.add(BorderLayout.SOUTH, del);
+        jFrame.setSize(500,500);
         jFrame.setVisible(true);
     }
 }
